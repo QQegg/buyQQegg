bd4ae376f3d9325f41c190bc1d5b8f5a2836105c
陳欣妤 <lovefish0416@gmail.com>
2018-01-19 11:16:41 +0800

    建立Controller(不喜歡的商品類別、評價評論)

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function create()
+    {
+    }
+    public function store()
+    {
+    }
+    public function edit()
+    {
+    }
+    public function update()
+    {
+    }
+    public function destroy()
+    {
+    }
+}
---
+++ app/Http/Controllers/DislikeCategoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DislikeCategoryController extends Controller
+{
+    public function create()
+    {
+    }
+    public function store()
+    {
+    }
+    public function edit()
+    {
+    }
+    public function update()
+    {
+    }
+    public function destroy()
+    {
+    }
+}
