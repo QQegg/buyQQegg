29b63e43bdf66107e00a065691bb75650b473eb1
陳欣妤 <lovefish0416@gmail.com>
2018-01-24 12:21:07 +0800

    套用到主樣板

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,29 @@
-<!DOCTYPE html>
+{{--<!DOCTYPE html>--}}
+{{--<html>--}}
+{{--<head>--}}
+    {{--<title>@yield('title')</title>--}}
+    {{--<!-- for-mobile-apps -->--}}
+    {{--<meta name="viewport" content="width=device-width, initial-scale=1">--}}
+    {{--<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />--}}
+    {{--<meta name="keywords" content="Seafaring Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,--}}
+{{--Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />--}}
+    {{--<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);--}}
+        {{--function hideURLbar(){ window.scrollTo(0,1); } </script>--}}
+    {{--<!-- //for-mobile-apps -->--}}
+    {{--<link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all" />--}}
+    {{--<link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css" media="all" />--}}
+    {{--<!-- js -->--}}
+    {{--<script src="{{asset('js/jquery-1.11.1.min.js')}}"></script>--}}
+    {{--<!-- //js -->--}}
+    {{--<link href='https://fonts.googleapis.com/css?family=Josefin+Sans:400,100,100italic,300,300italic,400italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>--}}
+    {{--<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>--}}
+{{--</head>--}}
+
+{{--<body>--}}
+        <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title')</title>
+    <title>Home</title>
     <!-- for-mobile-apps -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -10,11 +32,25 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
         function hideURLbar(){ window.scrollTo(0,1); } </script>
     <!-- //for-mobile-apps -->
-    <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all" />
-    <link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
     <!-- js -->
-    <script src="{{asset('js/jquery-1.11.1.min.js')}}"></script>
+    <script src="js/jquery-1.11.1.min.js"></script>
     <!-- //js -->
+    <!-- FlexSlider -->
+    <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+    <script defer src="js/jquery.flexslider.js"></script>
+    <script type="text/javascript">
+        $(window).load(function(){
+            $('.flexslider').flexslider({
+                animation: "slide",
+                start: function(slider){
+                    $('body').removeClass('loading');
+                }
+            });
+        });
+    </script>
+    <!-- //FlexSlider -->
     <link href='https://fonts.googleapis.com/css?family=Josefin+Sans:400,100,100italic,300,300italic,400italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
 </head>
