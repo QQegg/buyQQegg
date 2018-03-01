eed3fcbdf93d3495bed99bf575242bebca902235
陳欣妤 <lovefish0416@gmail.com>
2018-03-01 17:35:05 +0800

    新增_修改密碼_頁面-part2

--- resources/views/change_password.blade.php
+++ resources/views/change_password.blade.php
@@ -5,8 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">修改密碼</div>
-
+                <div class="panel-heading"><h2>{{ Auth::user()->account }} 修改密碼</h2></div>
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ url('change/password/ww') }}">
                         {{ csrf_field() }}
