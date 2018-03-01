e11e65d35de5ef5002f1d2a942316f9599c65da0
陳欣妤 <lovefish0416@gmail.com>
2018-03-01 17:37:14 +0800

    新增password controller (對應change_password.blade.php)及 change_password controller (更新密碼)

--- app/Http/Controllers/ChangeMemberController.php
+++ app/Http/Controllers/ChangeMemberController.php
@@ -26,11 +26,25 @@ class ChangeMemberController extends Controller
     {
         return view('contact');
     }
-
+    public function password()
+    {
+        return view('change_password');
+    }
     public function update(Request $request)
     {
         $User=User::find(Auth::user()->id);
         $User->update($request->all());
-        return back()->with('success','Password Changed');
+        return back()->with('success','修改成功');
+    }
+    public function change_password()
+    {
+        $User=User::find(Auth::user()->id);
+        if(Hash::check(Input::get('passwordold'),$User['password']) && Input::get('password') ==Input::get('password_confirmation')){
+            $User->password =bcrypt(Input::get('password'));
+            $User->save();
+            return back()->with('success','修改成功');
+        }
+        else
+            return back()->with('error','修改失敗');
     }
 }
