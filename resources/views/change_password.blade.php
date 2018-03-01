9508d024f0f968a6700432befdc40614a8ad072e
陳欣妤 <lovefish0416@gmail.com>
2018-03-01 17:34:28 +0800

    新增_修改密碼_頁面

---
+++ resources/views/change_password.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">修改密碼</div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ url('change/password/ww') }}">
+                        {{ csrf_field() }}
+                        @if(session('success'))
+                            <div class="alert alert-success">{{session('success')}}</div>
+                        @elseif(session('error'))
+                            <div class="alert alert-danger">{{session('error')}}</div>
+                        @endif
+                        {{--passwordold--}}
+                        <div class="form-group{{ $errors->has('passwordold') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">old 密碼</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="passwordold" required>
+
+                                @if ($errors->has('passwordold'))
+                                <span class="help-block">
+                                        <strong>{{ $errors->first('passwordold') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        {{--passwordnew--}}
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">new 密碼</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">請再次輸入密碼</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    修改密碼
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
