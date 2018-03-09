55838d4964e154ed77f0d349e0b828be538c0b5a
陳欣妤 <lovefish0416@gmail.com>
2018-03-09 15:33:05 +0800

    刪除不要的檔案(migration、model、Controller、web)

--- routes/web.php
+++ routes/web.php
@@ -23,22 +23,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'dislikecategory'], function() {
-    Route::get('/create',['as'=>'dccreate','uses'=>'DislikeCategoryController@create']);
-    Route::post('/store',['as' => 'dcstore' ,'uses'=>'DislikeCategoryController@store']);
-    Route::get('/edit/{id}',['as'=>'dcedit','uses'=>'DislikeCategoryController@edit']);
-    Route::patch('/update/{id}',['as'=>'dcupdate','uses'=>'DislikeCategoryController@update']);
-    Route::delete('/destroy/{id}',['as'=>'dcdestroy','uses'=>'DislikeCategoryController@destroy']);
-});
-
-Route::group(['prefix' => 'comment'], function() {
-    Route::get('/create',['as'=>'comcreate','uses'=>'CommentController@create']);
-    Route::post('/store',['as' => 'comstore' ,'uses'=>'CommentController@store']);
-    Route::get('/edit/{id}',['as'=>'comedit','uses'=>'CommentController@edit']);
-    Route::patch('/update/{id}',['as'=>'comupdate','uses'=>'CommentController@update']);
-    Route::delete('/destroy/{id}',['as'=>'comdestroy','uses'=>'CommentController@destroy']);
-});
-
 Route::group(['prefix' => 'comment'], function() {
     Route::get('/create',['as'=>'comcreate','uses'=>'CommentController@create']);
     Route::post('/store',['as' => 'comstore' ,'uses'=>'CommentController@store']);
