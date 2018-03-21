86bd96c758135c4a962c92ece10148b8f76cf056
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-03-21 20:26:37 +0800

    修改產品頁面的view

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -93,52 +93,14 @@
 			</div>
 			<div class="services-overview">
 				<h3>Services Overview</h3>
+				@foreach($product as $product)
 				<div class="services-overview-grids">
 					<div class="col-md-4 services-overview-grid">
 						<div class="services-overview-grd">
-							<img src="images/13.jpg" alt=" " class="img-responsive" />
+							<img src="{{env('BACKEND_URL') . $product->picture}}" alt=" " class="img-responsive" />
 							<div class="services-overview-gd">
-								<h4>Itaque earum rerum hic tenetur</h4>
-								<p>Neque porro quisquam est, qui dolorem ipsum quia dolor 
-									sit amet, consectetur, adipisci velit, sed quia non numquam 
-									eius modi tempora incidunt ut labore et dolore magnam aliquam 
-									quaerat voluptatem.</p>
-								<ul class="social-icons">
-									<li><a href="#" class="p"></a></li>
-									<li><a href="#" class="in"></a></li>
-									<li><a href="#" class="v"></a></li>
-									<li><a href="#" class="facebook"></a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-4 services-overview-grid">
-						<div class="services-overview-grd">
-							<img src="images/10.jpg" alt=" " class="img-responsive" />
-							<div class="services-overview-gd">
-								<h4>Itaque earum rerum hic tenetur</h4>
-								<p>Neque porro quisquam est, qui dolorem ipsum quia dolor 
-									sit amet, consectetur, adipisci velit, sed quia non numquam 
-									eius modi tempora incidunt ut labore et dolore magnam aliquam 
-									quaerat voluptatem.</p>
-								<ul class="social-icons">
-									<li><a href="#" class="p"></a></li>
-									<li><a href="#" class="in"></a></li>
-									<li><a href="#" class="v"></a></li>
-									<li><a href="#" class="facebook"></a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-4 services-overview-grid">
-						<div class="services-overview-grd">
-							<img src="images/9.jpg" alt=" " class="img-responsive" />
-							<div class="services-overview-gd">
-								<h4>Itaque earum rerum hic tenetur</h4>
-								<p>Neque porro quisquam est, qui dolorem ipsum quia dolor 
-									sit amet, consectetur, adipisci velit, sed quia non numquam 
-									eius modi tempora incidunt ut labore et dolore magnam aliquam 
-									quaerat voluptatem.</p>
+								<h4>商品名稱：{{$product->name}}</h4>
+								<p>商品規格：{{$product->specification}}</p>
 								<ul class="social-icons">
 									<li><a href="#" class="p"></a></li>
 									<li><a href="#" class="in"></a></li>
@@ -149,63 +111,7 @@
 						</div>
 					</div>
 					<div class="clearfix"> </div>
-				</div>
-				<div class="services-overview-grids">
-					<div class="col-md-4 services-overview-grid">
-						<div class="services-overview-grd">
-							<img src="images/8.jpg" alt=" " class="img-responsive" />
-							<div class="services-overview-gd">
-								<h4>Itaque earum rerum hic tenetur</h4>
-								<p>Neque porro quisquam est, qui dolorem ipsum quia dolor 
-									sit amet, consectetur, adipisci velit, sed quia non numquam 
-									eius modi tempora incidunt ut labore et dolore magnam aliquam 
-									quaerat voluptatem.</p>
-								<ul class="social-icons">
-									<li><a href="#" class="p"></a></li>
-									<li><a href="#" class="in"></a></li>
-									<li><a href="#" class="v"></a></li>
-									<li><a href="#" class="facebook"></a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-4 services-overview-grid">
-						<div class="services-overview-grd">
-							<img src="images/7.jpg" alt=" " class="img-responsive" />
-							<div class="services-overview-gd">
-								<h4>Itaque earum rerum hic tenetur</h4>
-								<p>Neque porro quisquam est, qui dolorem ipsum quia dolor 
-									sit amet, consectetur, adipisci velit, sed quia non numquam 
-									eius modi tempora incidunt ut labore et dolore magnam aliquam 
-									quaerat voluptatem.</p>
-								<ul class="social-icons">
-									<li><a href="#" class="p"></a></li>
-									<li><a href="#" class="in"></a></li>
-									<li><a href="#" class="v"></a></li>
-									<li><a href="#" class="facebook"></a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-4 services-overview-grid">
-						<div class="services-overview-grd">
-							<img src="images/6.jpg" alt=" " class="img-responsive" />
-							<div class="services-overview-gd">
-								<h4>Itaque earum rerum hic tenetur</h4>
-								<p>Neque porro quisquam est, qui dolorem ipsum quia dolor 
-									sit amet, consectetur, adipisci velit, sed quia non numquam 
-									eius modi tempora incidunt ut labore et dolore magnam aliquam 
-									quaerat voluptatem.</p>
-								<ul class="social-icons">
-									<li><a href="#" class="p"></a></li>
-									<li><a href="#" class="in"></a></li>
-									<li><a href="#" class="v"></a></li>
-									<li><a href="#" class="facebook"></a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div class="clearfix"> </div>
+					@endforeach
 				</div>
 			</div>
 		</div>
