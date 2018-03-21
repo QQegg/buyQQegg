b0532ececd3e422268e4c72c8de556564d604ea2
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-03-21 18:18:40 +0800

    新增產品model

---
+++ app/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable=[
+        'id',
+        'Category_id',
+        'store_id',
+        'name',
+        'specification',
+        'price',
+        'unit',
+        'picture',
+    ];
+
+    protected $table="commoditys";
+}
+}
