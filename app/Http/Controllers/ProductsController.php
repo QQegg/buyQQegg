f26f043e33d4c8614b814f6d3db0cb502349edef
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-03-22 15:01:03 +0800

    抓取產品資訊

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\Category;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
@@ -10,6 +11,12 @@ class ProductsController extends Controller
     public function index()
     {
         $product = Product::all();
+        $cc = 0;
+        foreach ($product as $count){
+            $category_name = Category::all()->where('id',$count['Category_id'])->pluck('name');
+            $product[$cc]['C_name'] = $category_name->first();
+            $cc++;
+        }
         return view('services', compact('product'));
     }
 }
