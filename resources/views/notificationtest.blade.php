d8af50678998e4fbadf354026f9fb9d6419bebbd
SKILL573 <a9101021249@gmail.com>
2018-04-09 15:43:10 +0800

    no message

--- resources/views/notificationtest.blade.php
+++ resources/views/notificationtest.blade.php
@@ -14,24 +14,24 @@
             //回傳相應的 通知數 通知標題 通知內容
         }
     </script>
-    <script> //timer countdown
-        // 標準時間
-        var countDownDate = new Date().getTime();
-        // Update the count down every 1 second
-        var x = setInterval(function() {
-            // 比較時間
-            var now = new Date().getTime();
-            // 時間比較
-            var distance = now - countDownDate;
-            // 如果有超過固定時間 進行推播更新
-            if (distance > 10) {
-                test();
-                distance=0;
-            }
-        },1000);
-    </script>
 </head>
 <body>
+<script> //timer countdown
+    // 標準時間
+    var countDownDate = new Date().getTime();
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+        // 比較時間
+        var now = new Date().getTime();
+        // 時間比較
+        var distance = now - countDownDate;
+        // 如果有超過固定時間 進行推播更新
+        if (distance > 10) {
+            test();
+            distance=0;
+        }
+    },1000);
+</script>
 <br/>
 <button onclick="test()">test</button>
 </body>
