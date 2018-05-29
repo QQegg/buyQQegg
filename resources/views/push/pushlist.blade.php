45c933d882666adb208f1cf268e91ce405855c07
陳美妤 <s3a432090@student.ncut.edu.tw>
2018-05-29 21:57:31 +0800

    促銷訊息列表功能

---
+++ resources/views/push/pushlist.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.master')
+<!-- header -->
+@section('title', 'services')
+<!-- //banner -->
+@section('content')
+<!-- services -->
+	<div class="services">
+		<div class="container">
+			<ol class="breadcrumb breadco">
+				<li><a href="#">Home</a></li>
+				<li class="active">促銷訊息列表</li>
+			</ol>
+
+			<div class="services-overview">
+				<h3>促銷訊息列表</h3>
+				<div class="services-overview-grids">
+					@foreach($push as $push)
+					<div class="col-md-4 services-overview-grid">
+						<div class="services-overview-grd">
+							<img src="{{env('BACKEND_URL3') . $push->picture}}" alt=" " class="img-responsive" />
+							<div class="services-overview-gd">
+								<h4>名稱：{{$push->title}}</h4>
+								<h4>店家：{{$push->Store_name}}</h4>
+								<a href="{{route('pushdetail',$push->id)}}" class="btn btn-success">觀看產品詳細資訊</a>
+								<ul class="social-icons">
+									<li><a href="#" class="p"></a></li>
+									<li><a href="#" class="in"></a></li>
+									<li><a href="#" class="v"></a></li>
+									<li><a href="#" class="facebook"></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					@endforeach
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+	</div>
+<!-- //services -->
+<!--footer-->
+
+<!--//footer-->
+@endsection
