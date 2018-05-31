690e09d455721f8a23227b94ac1f6850632709eb
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-05-31 18:10:04 +0800

    首頁修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,234 +1,40 @@
 @extends('layouts.master')
 <!-- header -->
 @section('content')
-<!-- //header -->
+    <!-- //header -->
 @section('content')
-<!-- banner -->
-	<div class="banner">
-		<div class="container">
-			<div class="banner-info-grid">
-				<section class="slider">
-					<div class="flexslider">
-						<ul class="slides">
-							<li>
-								<div class="banner-info">
-									<h1>Sed ut perspiciatis unde omnis iste natus</h1>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-										sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-									<div class="more">
-										<a href="single.html">More Info</a>
-									</div>
-								</div>
-							</li>
-							<li>
-								<div class="banner-info">
-									<h1>Sed ut perspiciatis unde omnis iste natus</h1>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-										sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-									<div class="more">
-										<a href="single.html">More Info</a>
-									</div>
-								</div>
-							</li>
-							<li>
-								<div class="banner-info">
-									<h1>Sed ut perspiciatis unde omnis iste natus</h1>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-										sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-									<div class="more">
-										<a href="single.html">More Info</a>
-									</div>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</section>
-			</div>
-		</div>
-	</div>
-<!-- //banner -->
-<div class="copyrights">Collect from <a href="http://www.cssmoban.com/"  title="网站模板">网站模板</a></div>
-<!-- banner-bottom -->
-	<div class="banner-bottom">
-		<div class="container">
-			<h3>Welcome To Our Travel Agency !</h3>
-			<div class="banner-bottom-grids">
-				<div class="col-md-6 banner-bottom-grid">
-					<div class="col-xs-8 banner-bottom-grid-left">
-						<h4>totam rem aperiam, eaque</h4>
-						<ul>
-							<li>Voluptatem <a href="#"><span class="glyphicon glyphicon-cloud" aria-hidden="true"></span></a></li>
-							<li>Aperiam <a href="#"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a></li>
-							<li><a href="#">Incididunt<span class="glyphicon glyphicon-off" aria-hidden="true"></span></a></li>
-						</ul>
-						<p>Quis autem vel eum iure reprehenderit qui in ea voluptate velit 
-							esse quam nihil.</p>
-						<div class="more m1">
-							<a href="single.html">More Info</a>
-						</div>
-					</div>
-					<div class="col-xs-4 banner-bottom-grid-right">
-						<img src="images/1.jpg" alt=" " class="img-responsive" />
-					</div> 
-					<div class="clearfix"> </div>
-				</div>
-				<div class="col-md-6 banner-bottom-grid">
-					<div class="col-xs-4 banner-bottom-grid-right">
-						<img src="images/2.jpg" alt=" " class="img-responsive" />
-					</div> 
-					<div class="col-xs-8 banner-bottom-grid-left banner-bottom-grid-l">
-						<h4>totam rem aperiam, eaque</h4>
-						<ul>
-							<li><span class="glyphicon glyphicon-cloud" aria-hidden="true"></span>Voluptatem <a href="#"></a></li>
-							<li><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>Aperiam <a href="#"></a></li>
-							<li><a href="#"><span class="glyphicon glyphicon-off" aria-hidden="true"></span>Incididunt</a></li>
-						</ul>
-						<p>Quis autem vel eum iure reprehenderit qui in ea voluptate velit 
-							esse quam nihil.</p>
-						<div class="more m1">
-							<a href="single.html">More Info</a>
-						</div>
-					</div>
-					<div class="clearfix"> </div>
-				</div>
-				<div class="clearfix"></div>
-			</div>
-			<div class="banner-bottom-grids">
-				<div class="col-md-6 banner-bottom-grid">
-					<div class="col-xs-8 banner-bottom-grid-left">
-						<h4>totam rem aperiam, eaque</h4>
-						<ul>
-							<li>Voluptatem <a href="#"><span class="glyphicon glyphicon-cloud" aria-hidden="true"></span></a></li>
-							<li>Aperiam <a href="#"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a></li>
-							<li><a href="#">Incididunt<span class="glyphicon glyphicon-off" aria-hidden="true"></span></a></li>
-						</ul>
-						<p>Quis autem vel eum iure reprehenderit qui in ea voluptate velit 
-							esse quam nihil.</p>
-						<div class="more m1">
-							<a href="single.html">More Info</a>
-						</div>
-					</div>
-					<div class="col-xs-4 banner-bottom-grid-right">
-						<img src="images/3.jpg" alt=" " class="img-responsive" />
-					</div> 
-					<div class="clearfix"> </div>
-				</div>
-				<div class="col-md-6 banner-bottom-grid">
-					<div class="col-xs-4 banner-bottom-grid-right">
-						<img src="images/4.jpg" alt=" " class="img-responsive" />
-					</div> 
-					<div class="col-xs-8 banner-bottom-grid-left banner-bottom-grid-l">
-						<h4>totam rem aperiam, eaque</h4>
-						<ul>
-							<li><span class="glyphicon glyphicon-cloud" aria-hidden="true"></span>Voluptatem <a href="#"></a></li>
-							<li><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>Aperiam <a href="#"></a></li>
-							<li><a href="#"><span class="glyphicon glyphicon-off" aria-hidden="true"></span>Incididunt</a></li>
-						</ul>
-						<p>Quis autem vel eum iure reprehenderit qui in ea voluptate velit 
-							esse quam nihil.</p>
-						<div class="more m1">
-							<a href="single.html">More Info</a>
-						</div>
-					</div>
-					<div class="clearfix"> </div>
-				</div>
-				<div class="clearfix"></div>
-			</div>
-		</div>
-	</div>
-<!-- //banner-bottom -->
-<!-- welcome-bottom -->
-	<div class="welcome-bottom">
-		<div class="container">
-			<div class="welcome-bottom-grids">
-				<div class="welcome-bottom-grid-l">
-					<div class="more">
-						<a href="single.html">More Info</a>
-					</div>
-				</div>
-				<div class="welcome-bottom-grid-left">
-					<h3>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet commodi consequatur</h3>
-					<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam 
-						corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.</p>
-				</div>
-				<div class="clearfix"> </div>
-			</div>
-		</div>
-	</div>
-<!-- //welcome-bottom -->
-<!-- features -->
-	<div class="features">
-		<div class="container">
-			<div class="features-grids">
-				<div class="col-md-6 features-grid-left">
-					<h3>commodi consequatur</h3>
-					<div class="features-grids1">
-						<h2>molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</h2>
-						<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, 
-							consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt 
-							ut labore et dolore magnam aliquam quaerat voluptatem.</p>
-						<div class="col-md-6 features-grids1-left">
-							<ul>
-								<li><a href="#">aliquam quaerat</a></li>
-								<li><a href="#">non numquam eius</a></li>
-								<li><a href="#">adipisci velit, sed</a></li>
-							</ul>
-						</div>
-						<div class="col-md-6 features-grids1-left">
-							<ul>
-								<li><a href="#">aliquam quaerat</a></li>
-								<li><a href="#">non numquam eius</a></li>
-								<li><a href="#">adipisci velit, sed</a></li>
-							</ul>
-						</div>
-						<div class="clearfix"> </div>
-						<div class="more m1">
-							<a href="single.html">More Info</a>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6 features-grid-right">
-					<h3>Testimonials</h3>
-					<div class="test1">
-						<div class="col-xs-8 test1-right">
-							<p>Adipisci velit, sed quia non numquam eius modi tempora incidunt 
-							ut labore et dolore magnam aliquam quaerat voluptatem.<span>Steffen Robert</span></p>
-						</div>
-						<div class="col-xs-4 test1-left">
-							<img src="images/3.png" alt=" " class="img-responsive" />
-						</div>
-						<div class="clearfix"> </div>
-					</div>
-					<div class="test11">
-						<div class="col-xs-4 test1-left">
-							<img src="images/2.png" alt=" " class="img-responsive" />
-						</div>
-						<div class="col-xs-8 test1-right">
-							<p>Adipisci velit, sed quia non numquam eius modi tempora incidunt 
-							ut labore et dolore magnam aliquam quaerat voluptatem.<span>James Mark</span></p>
-						</div>
-						<div class="clearfix"> </div>
-					</div>
-					<div class="test11">
-						<div class="col-xs-8 test1-right">
-							<p>Adipisci velit, sed quia non numquam eius modi tempora incidunt 
-							ut labore et dolore magnam aliquam quaerat voluptatem.<span>Williums</span></p>
-						</div>
-						<div class="col-xs-4 test1-left">
-							<img src="images/4.png" alt=" " class="img-responsive" />
-						</div>
-						<div class="clearfix"> </div>
-					</div>
-				</div>
-				<div class="clearfix"> </div>
-			</div>
-		</div>
-	</div>
-<!-- //features -->
-<!--footer-->
-
-<!--//footer-->	
-<!-- for bootstrap working -->
-
-<!-- //for bootstrap working -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div id="main-slider" class="main-slider flexslider">
+                    <ul class="slides">
+                        <li>
+                            <img src="{{env('BACKEND_URL5') . '6.jpg'}}" alt=""/>
+                            {{--<div class="flex-caption">--}}
+                            {{--<h3>Modern Design</h3>--}}
+                            {{--<p>Duis fermentum auctor ligula ac malesuada. Mauris et metus odio, in pulvinar urna</p>--}}
+                            {{--<a href="#" class="btn btn-theme">Learn More</a>--}}
+                            {{--</div>--}}
+                        </li>
+                        <li>
+                            <img src="{{env('BACKEND_URL5') . '4.jpg'}}" alt=""/>
+                            {{--<div class="flex-caption">--}}
+                            {{--<h3>Fully Responsive</h3>--}}
+                            {{--<p>Sodales neque vitae justo sollicitudin aliquet sit amet diam curabitur sed fermentum.</p>--}}
+                            {{--<a href="#" class="btn btn-theme">Learn More</a>--}}
+                            {{--</div>--}}
+                        </li>
+                        <li>
+                            <img src="{{env('BACKEND_URL5') . '5.jpg'}}" alt=""/>
+                            {{--<div class="flex-caption">--}}
+                            {{--<h3>Clean & Fast</h3>--}}
+                            {{--<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit donec mer lacinia.</p>--}}
+                            {{--<a href="#" class="btn btn-theme">Learn More</a>--}}
+                            {{--</div>--}}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
