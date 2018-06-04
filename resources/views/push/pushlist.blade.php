dcb8254e2993059203d728e912c86b5637b01ba1
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-06-04 21:52:00 +0800

    防護措施

--- resources/views/push/pushlist.blade.php
+++ resources/views/push/pushlist.blade.php
@@ -3,38 +3,43 @@
 @section('title', 'services')
 <!-- //banner -->
 @section('content')
-<!-- services -->
-	<div class="services">
-		<div class="container">
-			<ol class="breadcrumb breadco">
-				<li><a href="#">Home</a></li>
-				<li class="active">促銷訊息列表</li>
-			</ol>
+    <!-- services -->
+    <div class="services">
+        <div class="container">
+            <ol class="breadcrumb breadco">
+                <li><a href="#">Home</a></li>
+                <li class="active">促銷訊息列表</li>
+            </ol>
 
-			<div class="services-overview">
-				<h3>促銷訊息列表</h3>
-				<div class="services-overview-grids">
-					@foreach($push as $push)
-					<div class="col-md-4 services-overview-grid">
-						<div class="services-overview-grd">
-							<img src="{{env('BACKEND_URL') . $push->Product_picture}}" alt=" " class="img-responsive" />
-							<div class="services-overview-gd">
-								<h4>名稱：{{$push->title}}</h4>
-								<h4>店家：{{$push->Store_name}}</h4>
-                                <h4>產品：{{$push->Product_name}}</h4>
-                                <h4>原價：{{$push->Product_price}}</h4>
-								{{--<a href="{{route('pushdetail',$push->id)}}" class="btn btn-success">觀看產品詳細資訊</a>--}}
-							</div>
-						</div>
-					</div>
-					@endforeach
-					<div class="clearfix"> </div>
-				</div>
-			</div>
-		</div>
-	</div>
-<!-- //services -->
-<!--footer-->
+            <div class="services-overview">
+                <h3>促銷訊息列表</h3>
+                <div class="services-overview-grids">
+                    @foreach($push as $push)
+                        <div class="col-md-4 services-overview-grid">
+                            <div class="services-overview-grd">
+                                <img src="{{env('BACKEND_URL') . $push->Product_picture}}" alt=" "
+                                     class="img-responsive"/>
+                                <div class="services-overview-gd">
+                                    <h4>名稱：{{$push->title}}</h4>
+                                    <h4>店家：{{$push->Store_name}}</h4>
+                                    @if(empty($push['Product_name']))
+                                        <h4>此產品已不再店家</h4>
+                                    @else
+                                        <h4>產品：{{$push->Product_name}}</h4>
+                                        <h4>原價：{{$push->Product_price}}</h4>
+                                    @endif
+                                    {{--<a href="{{route('pushdetail',$push->id)}}" class="btn btn-success">觀看產品詳細資訊</a>--}}
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- //services -->
+    <!--footer-->
 
-<!--//footer-->
+    <!--//footer-->
 @endsection
